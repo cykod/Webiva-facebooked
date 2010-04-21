9fd8e953974b06757cb1e32014cfccf938fac7a6
Doug Youch <doug@cykod.com>
2010-04-21 15:53:48 +0000

    Can share photos as well.

--- app/models/facebooked/share/link/facebook.rb
+++ app/models/facebooked/share/link/facebook.rb
@@ -4,7 +4,7 @@ class Facebooked::Share::Link::Facebook < PostStream::Share::Link::Base
   def self.post_stream_link_handler_info
     {
       :name => 'Facebook Link Handler',
-      :post_types => ['media', 'image', 'link']
+      :post_types => ['image']
     }
   end
 
@@ -15,9 +15,18 @@ class Facebooked::Share::Link::Facebook < PostStream::Share::Link::Base
 
   def process_request(params, opts={})
     if self.link =~ /^http:\/\/www\.facebook\.com\//
-      if self.link =~ /album.php\?aid=(\d+)&id=(\d+)/
-        aid = $1
-        id = $2
+      if self.link =~ /album.php\?(.+)/
+        query = $1
+        aid = nil
+        id = nil
+        query.split('&').each do |nv|
+          name, value = nv.split('=')
+          id = value if name == 'id'
+          aid = value if name == 'aid'
+        end
+
+        return false unless id && aid
+
         client = Facebooked::AdminController.facebook_client
         if client.uid
           user = client.user
@@ -32,8 +41,35 @@ class Facebooked::Share::Link::Facebook < PostStream::Share::Link::Base
           return false if photos.empty?
 
           self.options.photo = photos[0]
-          self.options.album_url = "http://www.facebook.com/album.php?aid=#{aid}&id=#{id}"
+          self.options.link = album['link']
           self.options.uid = client.uid
+
+          self.post.post_type = 'image'
+          return true
+        end
+      elsif self.link =~ /photo.php\?(.+)/
+        query = $1
+        pid = nil
+        id = nil
+        query.split('&').each do |nv|
+          name, value = nv.split('=')
+          id = value if name == 'id'
+          pid = value if name == 'pid'
+        end
+
+        return false unless id && pid
+
+        client = Facebooked::AdminController.facebook_client
+        if client.uid
+          mini_photos = MiniFB::Photos.new client.session
+          photos = mini_photos.get 'pids' => "#{id}_#{pid}"
+          return false if photos.empty?
+
+          self.options.photo = photos[0]
+          self.options.link = photos[0]['link']
+          self.options.uid = client.uid
+
+          self.post.post_type = 'image'
           return true
         end
       end
@@ -46,10 +82,11 @@ class Facebooked::Share::Link::Facebook < PostStream::Share::Link::Base
     maxwidth = (opts[:maxwidth] || 340).to_i
     maxheight = opts[:maxheight] ? opts[:maxheight].to_i : nil
     title_length = (opts[:title_length] || 40).to_i
-    renderer.render_to_string :partial => '/facebooked/share/link/facebook', :locals => {:post => self.post, :options => self.options, :maxwidth => maxwidth, :maxheight => maxheight, :title_length => title_length}
+
+    renderer.render_to_string :partial => '/facebooked/share/link/facebook', :locals => {:post => self.post, :options => self.options, :maxwidth => maxwidth, :maxheight => maxheight, :title_length => title_length, :ajax => renderer.ajax?}
   end
 
   class Options < HashModel
-    attributes :photo => {}, :album_url => nil, :uid => nil
+    attributes :photo => {}, :link => nil, :uid => nil
   end
 end
