af8663a512416785ef71de1d2cdd10cb2aa7d8bf
Doug Youch <doug@cykod.com>
2011-01-05 18:45:28 -0500

    when there is a signed request, make sure it is the same user that is already logged in

--- app/controllers/facebooked/app_renderer.rb
+++ app/controllers/facebooked/app_renderer.rb
@@ -7,13 +7,23 @@ class Facebooked::AppRenderer < ParagraphRenderer
 
   def login
     return render_paragraph :text => 'Facebook Application Login' if editor?
-    return render_paragraph :nothing => true if self.provider.logged_in?
-
-    Rails.logger.error login_html
+    return render_paragraph :nothing => true if self.logged_in?
     data_paragraph :type => 'text/html', :text => login_html
   end
 
   protected
+
+  def logged_in?
+    if params[:signed_request]
+      signed_request = Facebooked::SignedRequest.new params[:signed_request]
+      if ! signed_request.valid? || signed_request.user_id.nil? || self.provider.provider_id.nil? || signed_request.user_id != self.provider.provider_id
+        self.process_logout
+        return false
+      end
+    end
+
+    self.provider.logged_in?
+  end
 
   def provider
     @provider ||= Facebooked::OauthProvider.new session
