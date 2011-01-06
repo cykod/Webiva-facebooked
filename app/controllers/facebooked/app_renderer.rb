5308508ac08f6aab80397905046f8e56f800767e
Doug Youch <doug@cykod.com>
2011-01-06 10:21:40 -0500

    redirect user to the page they originally requested

--- app/controllers/facebooked/app_renderer.rb
+++ app/controllers/facebooked/app_renderer.rb
@@ -7,7 +7,18 @@ class Facebooked::AppRenderer < ParagraphRenderer
 
   def login
     return render_paragraph :text => 'Facebook Application Login' if editor?
-    return render_paragraph :nothing => true if self.logged_in?
+
+    if self.logged_in?
+      return render_paragraph(:nothing => true) unless session[:lock_lockout]
+
+      lock_lockout = session[:lock_lockout]
+      session[:lock_lockout] = nil
+      return render_paragraph(:nothing => true) if self.lock_lockout_url == lock_lockout
+
+      return redirect_paragraph lock_lockout
+    end
+
+    session[:lock_lockout] = self.lock_lockout_url
     data_paragraph :type => 'text/html', :text => login_html
   end
 
@@ -15,11 +26,9 @@ class Facebooked::AppRenderer < ParagraphRenderer
 
   def logged_in?
     if params[:signed_request]
+      # make sure the signed_request user and the user logged in with oauth are the same user
       signed_request = Facebooked::SignedRequest.new params[:signed_request]
-      if ! signed_request.valid? || signed_request.user_id.nil? || self.provider.provider_id.nil? || signed_request.user_id != self.provider.provider_id
-        self.process_logout
-        return false
-      end
+      return false unless signed_request.valid? && signed_request.user_id && self.provider.logged_in? && self.provider.provider_id && signed_request.user_id == self.provider.provider_id
     end
 
     self.provider.logged_in?
@@ -39,5 +48,14 @@ top.location = "#{url_for :controller => '/facebooked/client', :action => 'login
 </head>
 </html>
     LOGIN
+  end
+
+  # removes the signed_request from the url
+  def lock_lockout_url
+    url = request.request_uri
+    url, query = url.split '?'
+    query = query.split('&').map { |p| p.match(/^signed_request=/) ? nil : p }.compact.join('&') unless query.blank?
+    url = "#{url}?#{query}" unless query.blank?
+    url
   end
 end
