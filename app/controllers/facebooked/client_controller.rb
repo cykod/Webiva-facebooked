26587388681e891fe25d6ec71469c37895fb8fd4
Doug Youch <doug@cykod.com>
2011-04-27 09:43:39 -0400

    Mark a facebook user as unsubscribed when they deauthorize your app

--- app/controllers/facebooked/client_controller.rb
+++ app/controllers/facebooked/client_controller.rb
@@ -1,6 +1,17 @@
 
 class Facebooked::ClientController < Oauth::ClientController
 
+  skip_before_filter :verify_authenticity_token, :only => ['deauthorize']
+
+  def deauthorize
+    signed_request = Facebooked::SignedRequest.new params[:signed_request]
+    if signed_request.valid?
+      oauth_user = OauthUser.first :conditions => {:provider => 'facebook', :provider_id => signed_request.data['user_id']}
+      oauth_user.end_user.unsubscribe if oauth_user && oauth_user.end_user
+    end
+    render :nothing => true
+  end
+
   protected
 
   def provider
