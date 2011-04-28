8aea12d517f17d954390bb776bae2ac191d04099
Doug Youch <doug@cykod.com>
2011-04-28 15:05:54 -0400

    find a facebook friends who registered before you

--- app/controllers/facebooked/app_controller.rb
+++ app/controllers/facebooked/app_controller.rb
@@ -4,6 +4,13 @@ class Facebooked::AppController < ParagraphController
   editor_header 'Facebooked Application Paragraphs'
 
   editor_for :login, :name => "Login", :no_options => true
-
+  editor_for :friend_rewards, :name => 'Friend Rewards'
+  
   class LoginOptions < HashModel; end
+  
+  class FriendRewardsOptions < HashModel
+    
+    options_form(
+                 )
+  end
 end
--- app/controllers/facebooked/app_feature.rb
+++ app/controllers/facebooked/app_feature.rb
@@ -1,3 +1,20 @@
 
 class Facebooked::AppFeature < ParagraphFeature
+  feature :facebooked_app_friend_rewards, :default_feature => <<-FEATURE
+  <cms:friends>
+  <ul>
+    <cms:friend>
+    <li><cms:image/> <cms:name/></li>
+    </cms:friend>
+  </ul>
+  </cms:friends>
+  FEATURE
+
+  def facebooked_app_friend_rewards_feature(data)
+    webiva_feature(:facebooked_app_friend_rewards,data) do |c|
+      c.loop_tag('friend') { |t| data[:friends] }
+      c.h_tag('friend:name') { |t| t.locals.friend.name }
+      c.image_tag('friend:image') { |t| t.locals.friend.end_user.image if t.locals.friend.end_user }
+    end
+  end
 end
